b3c0812b98e64d9fd7c48fbde458abb4bd4e7d64
Umer Masood <umermasood.dev@gmail.com>
2022-09-26 15:40:33 +0500

    Solved 01-exercise/01-goroutines/01-hello

--- 01-exercise/01-goroutines/01-hello/main.go
+++ 01-exercise/01-goroutines/01-hello/main.go
@@ -19,12 +19,19 @@ func main() {
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
+	go fun("goroutine-1")
 
 	// goroutine with anonymous function
+	go func() {
+		fun("goroutine-2")
+	}()
 
 	// goroutine with function value call
+	fv := fun
+	go fv("goroutine-3")
 
 	// wait for goroutines to end
+	time.Sleep(100 * time.Millisecond)
 
 	fmt.Println("done..")
 }
