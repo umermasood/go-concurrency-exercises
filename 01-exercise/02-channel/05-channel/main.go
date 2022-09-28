feef045fecf02aec9fedaebaf9ca8db1ce698aee
Umer Masood <umermasood.dev@gmail.com>
2022-09-28 17:44:16 +0500

    Solved 01-exercise/02-channel/05-channel

--- 01-exercise/02-channel/05-channel/main.go
+++ 01-exercise/02-channel/05-channel/main.go
@@ -6,6 +6,16 @@ func main() {
 	//TODO: create channel owner goroutine which return channel and
 	// writes data into channel and
 	// closes the channel when done.
+	owner := func() <-chan int {
+		ch := make(chan int)
+		go func() {
+			defer close(ch)
+			for i := 0; i < 5; i++ {
+				ch <- i
+			}
+		}()
+		return ch
+	}
 
 	consumer := func(ch <-chan int) {
 		// read values from channel
