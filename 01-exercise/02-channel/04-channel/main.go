7916d26798746c9bb393eeaf5e7591a5fb85a094
Umer Masood <umermasood.dev@gmail.com>
2022-09-28 16:39:38 +0500

    Solved 01-exercise/02-channel/04-channel

--- 01-exercise/02-channel/04-channel/main.go
+++ 01-exercise/02-channel/04-channel/main.go
@@ -1,20 +1,30 @@
 package main
 
-// TODO: Implement relaying of message with Channel Direction
+import "fmt"
 
-func genMsg() {
+// Implement relaying of message with Channel Direction
+
+func genMsg(ch1 chan<- string) {
 	// send message on ch1
+	ch1 <- "message"
 }
 
-func relayMsg() {
-	// recv message on ch1
-	// send it on ch2
+func relayMsg(ch1 <-chan string, ch2 chan<- string) {
+	//msg := <-ch1 // receive message on ch1
+	//ch2 <- msg   // send it on ch2
+	//OR
+	ch2 <- <-ch1 // Alternative one liner
 }
 
 func main() {
 	// create ch1 and ch2
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
-	// spine goroutine genMsg and relayMsg
+	// spin goroutine genMsg and relayMsg
+	go genMsg(ch1)
+	go relayMsg(ch1, ch2)
 
-	// recv message on ch2
+	// receive message on ch2
+	fmt.Printf("Recieved relayed message: %q\n", <-ch2)
 }
